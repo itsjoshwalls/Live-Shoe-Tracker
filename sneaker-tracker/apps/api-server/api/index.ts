e5ada2bc69c085a93d15737222fc3439eecd681e
Joshua Walls <itsjoshwalls@gmail.com>
2025-11-19 12:01:55 -0500

    Fix serverless function handler - remove async
    Express middleware should not be async in Vercel handler

--- sneaker-tracker/apps/api-server/api/index.ts
+++ sneaker-tracker/apps/api-server/api/index.ts
@@ -2,6 +2,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/server';
 
 // Export the Express app as a Vercel serverless function
-export default async (req: VercelRequest, res: VercelResponse) => {
+export default (req: VercelRequest, res: VercelResponse) => {
   return app(req, res);
 };
